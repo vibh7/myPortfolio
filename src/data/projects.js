f0ff9f76e40da1c8da126d93f6c00040a9631994
vikas <vikasbhartilm10@gmail.com>
2025-11-13 09:04:51 +0530

    modified static path

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,7 +5,7 @@ export default [
     description: "dWallet is a full-stack digital wallet application that empowers users to securely register, log in, manage balances, and perform fast money transfers. Built with React and Spring Boot, it features real-time transaction analytics, JWT-based authentication, and intuitive dashboards for a modern fintech experience.",
     github: "https://github.com/vibh7/digital-wallet",
     live: "https://digital-wallet-psi.vercel.app/",
-    image: "src/assets/images/dWallet.png",
+    image: "/images/dWallet.png", // move dWallet.png to /public/images/
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ export default [
     description: "Developed a dynamic full-stack web application featuring advanced search, group chat, real-time messaging, and secure file sharing to enhance user collaboration and communication.",
     github: "https://github.com/vibh7/VChat",
     live: "https://vchat-smoky.vercel.app/chat",
-    image: "src/assets/images/VChat.png",
+    image: "/images/VChat.png", // move VChat.png to /public/images/
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ export default [
     description: "A movie discovery platform with personalized recommendations and user reviews based on the movies you add in watchlist.",
     github: "https://github.com/vibh7/filmvault",
     live: "https://filmwault.netlify.app/",
-    image: "src/assets/images/FilmVault.png",
+    image: "/images/FilmVault.png", // move FilmVault.png to /public/images/
   },
   {
     id: 4,
@@ -29,6 +29,6 @@ export default [
     description: "A simple and elegant web design for a villa rental service, showcasing available properties with images and descriptions.",
     github: "https://github.com/vikasbharti/villa-web-design",
     live: "https://villa-web-design.vercel.app",
-    image: "src/assets/images/villa.png",
+    image: "/images/villa.png", // move villa.png to /public/images/
   }
 ];
